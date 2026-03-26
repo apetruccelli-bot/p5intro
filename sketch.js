1b2322acb2d0d629a4fb78fc57a2fd6fbbae72c6
anisia26 <anisia.petruccelli.21@um.edu.mt>
2026-03-26 16:15:06 +0100

    images colage

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
-function setup() {
-  createCanvas(400, 400);
+let topo;
+function preload() {  
+ topo = loadImage('Topo_Gigio.jpg');
+
 }
 
-function draw() {
-  background(220);
+
+function setup() {
+  createCanvas(400, 400);
+background  (160,0,0);
+
+
+let x;
+let y;
+
+for (let i=0; i<10; i = i+1){
+  
+  let x = random (300);
+  let y = random (300);
+ image(topo, x, y, 30, 30);
+
+}
+
+
 }
